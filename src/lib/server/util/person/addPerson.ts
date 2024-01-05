d1f502cf9814b5aeec4f6b1951419009713ac17e
MangoSwirl <collin.camerono@gmail.com>
2024-01-05 14:11:22 -0800

    Apply default permissions

--- src/lib/server/util/person/addPerson.ts
+++ src/lib/server/util/person/addPerson.ts
@@ -1,6 +1,16 @@
 import type { Role } from '$lib/util/person/role/roles';
+import { defaultPermissions } from '../permission/defaultPermissions';
 import prisma from '../prisma';
 
 export async function addPerson(name: string, email: string, role: Role, teamAffiliated: boolean) {
-	return await prisma.person.create({ data: { name, email, role, teamAffiliated } });
+	const person = await prisma.person.create({ data: { name, email, role, teamAffiliated } });
+
+	await prisma.permission.createMany({
+		data: defaultPermissions.map((permission) => ({
+			personId: person.id,
+			path: permission,
+		}))
+	});
+
+	return person;
 }
