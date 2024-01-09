fb7633adb3231135686ee0942219cc33459c0eb5
MangoSwirl <collin.camerono@gmail.com>
2024-01-08 18:10:51 -0800

    Add supervisor role

--- src/lib/util/person/role/localized.ts
+++ src/lib/util/person/role/localized.ts
@@ -4,5 +4,6 @@ export const localizedRole = (role: RoleString) =>
 	({
 		STUDENT: 'Student',
 		MENTOR: 'Mentor',
+		SUPERVISOR: 'Supervisor',
 		OTHER: 'Other',
 	}[role] ?? role);
--- src/lib/util/person/role/roles.ts
+++ src/lib/util/person/role/roles.ts
@@ -1,6 +1,7 @@
 export const roles = [
     "STUDENT",
     "MENTOR",
+    "SUPERVISOR",
     "OTHER",
 ]
 
