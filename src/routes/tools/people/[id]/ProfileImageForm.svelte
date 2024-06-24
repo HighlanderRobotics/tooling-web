ded8aef47b147c4f7a8b3c8e2d1d983059fc8983
MangoSwirl <collin.camerono@gmail.com>
2024-06-23 22:57:53 -0700

    Make the dialog look nice

--- src/routes/tools/people/[id]/ProfileImageForm.svelte
+++ src/routes/tools/people/[id]/ProfileImageForm.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { Button } from 'magnolia-ui-svelte';
+	import Dropzone from 'svelte-file-dropzone';
 
 	export let dialogOpen: boolean;
 	let saving = false;
 	let errorMessage: string | null;
+
+	let form: HTMLFormElement;
 </script>
 
 <form
+	bind:this={form}
 	on:submit={() => (saving = true)}
 	use:enhance={() => {
 		console.log('ay');
@@ -30,23 +34,62 @@
 	method="post"
 	enctype="multipart/form-data"
 >
-	<input type="file" name="image" required />
-	<div>
-		{#if errorMessage}
+	<h1>Upload an image</h1>
+	<p>
+		Use a photograph containing this person's face on a neutral backdrop. These are used for the
+		website and badges, so make sure they're recognizable.
+	</p>
+	<Dropzone
+		required
+		disableDefaultStyles
+		containerClasses="styled-dropzone"
+		name="image"
+		accept="image/jpeg"
+		multiple={false}
+		disabled={saving}
+		on:drop={() => {
+			form.requestSubmit();
+			form.reset();
+		}}
+	>
+		{#if saving}
+			<p>Uploading...</p>
+		{:else if errorMessage}
 			<p class="error">{errorMessage}</p>
+		{:else}
+			<p>Drop photo here or click to browse</p>
 		{/if}
+	</Dropzone>
+	<div>
 		<Button
 			type="button"
 			disabled={saving}
 			variant="secondary"
-			on:click={() => (dialogOpen = false)}>Cancel</Button
+			on:click={() => (dialogOpen = false)}
 		>
-		<Button type="submit" disabled={saving}>Save</Button>
+			Cancel
+		</Button>
 	</div>
 </form>
 
 <style>
 	.error {
 		color: var(--danger);
+	}
+
+	p {
+		color: var(--body);
+		margin-bottom: 14px;
+	}
+
+	:global(.styled-dropzone) {
+		background-color: var(--secondary-container);
+		height: 150px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 7px;
+		border: 2px solid var(--light-gray);
+		margin-bottom: 14px;
 	}
 </style>
