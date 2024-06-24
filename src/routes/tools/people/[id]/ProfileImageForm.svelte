723d8d18eb1c8bd8790f4e2627b6fdb0d5b7e8ce
MangoSwirl <collin.camerono@gmail.com>
2024-06-23 20:15:52 -0700

    Add proper form resolution

--- src/routes/tools/people/[id]/ProfileImageForm.svelte
+++ src/routes/tools/people/[id]/ProfileImageForm.svelte
@@ -3,15 +3,50 @@
 	import { Button } from 'magnolia-ui-svelte';
 
 	export let dialogOpen: boolean;
+	let saving = false;
+	let errorMessage: string | null;
 </script>
 
-<form use:enhance action="?/uploadImage" method="post" enctype="multipart/form-data">
+<form
+	on:submit={() => (saving = true)}
+	use:enhance={() => {
+		console.log('ay');
+		return async ({ result, update }) => {
+			saving = false;
+
+			if (result.type === 'success') {
+				dialogOpen = false;
+			}
+
+			if (result.type === 'failure') {
+				errorMessage =
+					typeof result.data?.message === 'string' ? result.data.message : 'Failed to save image';
+			}
+
+			update();
+		};
+	}}
+	action="?/uploadImage"
+	method="post"
+	enctype="multipart/form-data"
+>
 	<input type="file" name="image" required />
 	<div>
-		<Button type="button" variant="secondary" on:click={() => (dialogOpen = false)}>Cancel</Button>
-		<Button type="submit">Save</Button>
+		{#if errorMessage}
+			<p class="error">{errorMessage}</p>
+		{/if}
+		<Button
+			type="button"
+			disabled={saving}
+			variant="secondary"
+			on:click={() => (dialogOpen = false)}>Cancel</Button
+		>
+		<Button type="submit" disabled={saving}>Save</Button>
 	</div>
 </form>
 
 <style>
+	.error {
+		color: var(--danger);
+	}
 </style>
